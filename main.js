4ffdf21d4c881ed7537bd5d97bfd52ca5f16fb22
mahichy <mohiuddinoshim@yahoo.com>
2022-10-17 03:15:53 +0600

    first commit problem

---
+++ main.js
@@ -0,0 +1,22 @@
+// alert('Ohsim');
+console.log("Hello World!")
+console.error("this is na error ")
+console.warn("this is a warning")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.write("Hello World from javascript!")
